02c67bc020a1267b003522fb21330448bb5d8390
miracatici <miracatici@gmail.com>
2016-01-18 04:42:44 +0200

    Refactoring

--- note_model/NoteModel.py
+++ note_model/NoteModel.py
@@ -7,29 +7,29 @@ from tonic_identifier.tonic_identifier import TonicLastNote
 
 
 class NoteModel:
-    performedNotes = {}
 
-    def __init__(self, distribution, tonichz, makam):
-            self.distribution = distribution
-            self.makam = makam
-            self.tonicHz = tonichz
+    def __init__(self):
+        pass
 
-    def calculate_notes(self):
+    @staticmethod
+    def calculate_notes(distribution, tonichz, makam):
 
-        note_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data', 'note_dict.json')
+        note_file = os.path.join(os.path.dirname(os.path.abspath("note_dict.json")), 'note_model/data', 'note_dict.json')
         note_dict = json.load(open(note_file, 'r'))
-        makam_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data', 'makam_extended.json')
+        makam_file = os.path.join(os.path.dirname(os.path.abspath("makam_extended.json")), 'note_model/data', 'makam_extended.json')
         makam_extended = json.load(open(makam_file, 'r'))
-        note_symbol = makam_extended[self.makam]["karar_symbol"]
-        tonic_cent = hz_to_cent(self.tonicHz, 8.17579892 * 2)[0]
+        note_symbol = makam_extended[makam]["karar_symbol"]
+        c0 = 16.35
+        tonic_cent = hz_to_cent(tonichz, c0)[0]
         teoretical_cent = int(note_dict[note_symbol][0])
         ratio = teoretical_cent / tonic_cent
-        peaks = self.distribution.detect_peaks()
+        peaks = distribution.detect_peaks()
         peakId = peaks[0]
-        stable_pitches = self.distribution.bins[peakId]
-        stable_pitches_cent = hz_to_cent(stable_pitches, 8.17579892 * 2)
+        stable_pitches = distribution.bins[peakId]
+        stable_pitches_cent = hz_to_cent(stable_pitches, c0)
         stable_pitches_cent_norm = stable_pitches_cent * ratio
         teonotefreq = []
+        performedNotes = {}
 
         for key in note_dict.keys():
             teonotefreq.append(int(note_dict[key][0]))
@@ -39,8 +39,8 @@ class NoteModel:
                 if int(note_dict[i][0]) == temp:
                     note = u''.join(note_dict[i][1]).encode('utf-8').strip()
                     print note
-                    self.performedNotes[note] = {}
+                    performedNotes[note] = {}
 
-        return self.performedNotes
+        return performedNotes
 
 
