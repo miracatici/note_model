de9a75eab39cfd9e19e5c66ea1c24fda492ce053
miracatici <miracatici@gmail.com>
2016-01-18 12:13:00 +0200

    Package installation

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='makamnotemodel',
+      version='0.1',
+      description='An Automatic Note Modelling Method for Turkish Makam Music Recordings',
+      author='Bilge Mirac Atici',
+      url='https://github.com/miracatici/note_model.git',
+      packages=['note_model'], requires=['tonic_identifier']
+      )
