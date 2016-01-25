47c296fc9f63da2e90a2d95aa754809b508f1737
Sertan Senturk <contact@sertansenturk.com>
2016-01-25 02:27:08 +0100

    Updated the note model

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ setup(name='note_model',
       version='0.1',
       description='An Automatic Note Modelling Method for Turkish Makam Music Recordings',
       author='Bilge Mirac Atici',
+      author_email='miracatici AT gmail DOT com',
+      license='agpl 3.0',
       url='https://github.com/miracatici/note_model.git',
-      packages=['note_model'], requires=['tonic_identifier']
+      packages=['note_model']
       )
