827783e66c8016fbaabb8b9b7058cfaf2addb4b4
Sertan Senturk <contact@sertansenturk.com>
2016-03-15 23:03:16 +0100

    Closes #5

--- note_model/NoteModel.py
+++ note_model/NoteModel.py
@@ -10,6 +10,8 @@ import matplotlib.ticker
 
 
 class NoteModel:
+    note_letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+
     def __init__(self, pitch_threshold=50):
         self.pitch_threshold = pitch_threshold
 
@@ -24,37 +26,8 @@ class NoteModel:
         except AttributeError:  
             pass
             
-        # Reading dictionary which contains note symbol, theoretical names and
-        # their cent values
-        note_file = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                                 'data', 'note_dict.json')
-        note_dict = json.load(open(note_file, 'r'))
-
-        # Reading dictionary which contains theoretical information about each
-        # makam
-        makam_file = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                                  'data', 'makam_extended.json')
-        makam_extended = json.load(open(makam_file, 'r'))
-
-        # get the tonic symbol of the makam
-        tonic_symbol = makam_extended[makam]["karar_symbol"]
-        
-        # recompute the cent values according to the tonic
-        theoretical_intervals = {}
-        for key, note in note_dict.iteritems():
-            theoretical_intervals[key] = note['Value'] - \
-                                         note_dict[tonic_symbol]['Value']
-            
-        # Temporary fix; the note symbols with b1, b8, #1, #8 are much less
-        # frequent than the naturals. However because the theoretical
-        # interval is so close to the natural these peaks are more to be
-        # selected than the natural. To avoid this, we are removing the "b1"
-        # and "#1"s from the search space for now. The code below should be
-        # removed when this issue (#5) is fixed
-        for key in theoretical_intervals.keys():
-            is_one_comma = any(a in key for a in ['b1', 'b8', '#1', '#8'])
-            if is_one_comma:
-                theoretical_intervals.pop(key, None)
+        theoretical_intervals = self._get_theoretical_intervals_to_search(
+            makam)
 
         # Calculate stable pitches
         peak_idx, peak_heights = distribution.detect_peaks()
@@ -86,6 +59,93 @@ class NoteModel:
                         break
                 
         return stable_notes
+
+    @staticmethod
+    def _is_same_pitch_class(n1, n2):
+        return (n1[0] + n1[2:]) == (n2[0] + n2[2:])
+
+    @staticmethod
+    def _get_close_natural_notes(key_signature):
+        close_near_flat = [ks[:2] for ks in key_signature if ks[3] == '1']
+
+        return close_near_flat
+
+    def _get_theoretical_intervals_to_search(self, makam):
+        # Reading dictionary which contains note symbol, theoretical names and
+        # their cent values
+        note_file = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                 'data', 'note_dict.json')
+        note_dict = json.load(open(note_file, 'r'))
+
+        # Reading dictionary which contains theoretical information about each
+        # makam
+        makam_file = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                  'data', 'makam_extended.json')
+        makam_extended = json.load(open(makam_file, 'r'))
+
+        # get the key signature
+        key_signature = makam_extended[makam]["key_signature"]
+        for note in note_dict.keys():  # extend the key signature to octaves
+            if len(note) > 2 and note not in key_signature and \
+                    any([self._is_same_pitch_class(note, ks)
+                         for ks in key_signature]):
+                key_signature.append(note)
+
+        close_natural_notes = self._get_close_natural_notes(key_signature)
+
+        # get the natural notes that are not close to the key signature
+        natural_notes = [n for n in note_dict.keys() if len(n) == 2 and n
+                         not in close_natural_notes]
+
+        # Remove the notes neighboring the scale notes
+        keep_notes = natural_notes + key_signature
+        for note_name in note_dict.keys():
+            if note_name in keep_notes:
+                if note_name[2:4] in ['b5', 'b4']:
+                    rm_note_symbol = self.note_letters[
+                        (self.note_letters.index(note_name[0]) - 1) % 7]
+                    rm_octave = note_name[1]
+                    rm_notes = [rm_note_symbol + rm_octave + '#' + c
+                                for c in ['4', '5']]
+                elif note_name[2:4] in ['#5', '#4']:
+                    rm_note_symbol = self.note_letters[
+                        (self.note_letters.index(note_name[0]) + 1) % 7]
+                    rm_octave = note_name[1]
+                    rm_notes = [rm_note_symbol + rm_octave + 'b' + c
+                                for c in ['4', '5']]
+                else:
+                    rm_notes = []
+
+                for rm_note in rm_notes:
+                    note_dict.pop(rm_note, None)
+
+            elif len(note_name) == 2:  # there is a near accidental already
+                note_dict.pop(note_name, None)
+            elif len(note_name) == 4:
+                if note_name[3] == '1':  # there is a near natural already
+                    note_dict.pop(note_name, None)
+                elif note_name[3] == '4':
+                    if note_name[:3] + '5' in keep_notes:
+                        note_dict.pop(note_name, None)
+                    else:
+                        pass
+                elif note_name[3] == '5':  # remove the 5 commas
+                    # if they are not in the kept notes
+                    note_dict.pop(note_name, None)
+                elif note_name[3] == '8':  # remove the 8 commas
+                    # if they are not in the kept notes
+                    note_dict.pop(note_name, None)
+                else:
+                    raise ValueError('Unhandled comma in ' + note_name)
+
+        # recompute the cent values according to the tonic
+        tonic_symbol = makam_extended[makam]["karar_symbol"]
+        theoretical_intervals = {}
+        for key, note in note_dict.iteritems():
+            theoretical_intervals[key] = note['Value'] - \
+                                         note_dict[tonic_symbol]['Value']
+
+        return theoretical_intervals
 
     @staticmethod
     def plot(distribution, stable_notes):
@@ -133,7 +193,7 @@ class NoteModel:
             txt_y_val = peak_val + 0.03 * max(distribution.vals)  # lift the
             # text a little bit
             ax.text(note['stable_pitch']['value'], txt_y_val, note_symbol,
-                    style='italic', verticalalignment='bottom', rotation=45)
+                    style='italic', verticalalignment='bottom', rotation=60)
             
         # define ylim higher than the highest peak so the note names have space
         plt.ylim([0, 1.2*max(distribution.vals)])
